d29bb84431768937926761c5abbacef369bade94
anasalkasem <anass111173@gmail.com>
2025-04-29 01:01:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import asyncio
 import logging
-from aiogram import Bot
+from aiogram import Bot, Dispatcher, executor
 from config import BOT_TOKEN
-from bot.handlers import dp
-from bot import database
+from bot import handlers, database
 
 logging.basicConfig(level=logging.INFO)
 
 database.init_db()
 
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot)
 
-async def main():
-    await dp.start_polling(bot)
+handlers.register_handlers(dp)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    executor.start_polling(dp, skip_updates=True)
